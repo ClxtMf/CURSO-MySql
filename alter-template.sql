60e4fa3ac0c1958e1726e4309790df1c059b9635
Olá, Sou Calixto!!! <calistomarcio6@gmail.com>
2022-06-29 19:26:22 -0300

    master

---
+++ alter-template.sql
@@ -0,0 +1,3 @@
+-- Active: 1656536586115@@127.0.0.1@3306@escola
+ALTER TABLE professores 
+    ADD COLUMN [column] [type] COMMENT '';
---
+++ create-table-template.sql
@@ -0,0 +1,7 @@
+-- Active: 1656536586115@@127.0.0.1@3306@escola
+CREATE TABLE table_name(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    create_time DATETIME COMMENT 'Create Time',
+    update_time DATETIME COMMENT 'Update Time',
+    content VARCHAR(255) COMMENT 'content'
+) DEFAULT CHARSET UTF8 COMMENT 'newTable';
